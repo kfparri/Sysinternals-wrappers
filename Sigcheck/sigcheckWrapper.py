5c70496c58ff62b0ba7241c8b48c9a1f4624e683
Kyle Parrish <kfparri@gmail.com>
2016-04-12 16:07:00 -0500

    I think I have the python version of SigCheck Working correctly, I'm now ready to test it in depth

--- Sigcheck/sigcheckWrapper.py
+++ Sigcheck/sigcheckWrapper.py
@@ -18,82 +18,150 @@ Changelog
 from subprocess import Popen, PIPE
 import os
 import datetime
-import filecmp
+import hashlib
+import wx
 
 REF_FILE = "ref.csv"
 LOG_FILE = "sigcheckWrapper.log"
 RESULTS_FILE = "results.csv"
+BUFF_SIZE = 65536
+TRAY_TOOLTIP = 'SigCheck has found a discrepancy with your computer certificates!'
+TRAY_ICON = 'icon.png'
 
 
-def writeLog(text):
+def writelog(text, logger):
     logger.write(str(datetime.datetime.now()) + ": " + text + '\n')
 
-# first, check to see if there is a log file with the same name as LOG_FILE,
-#  if there is, rename it to *.log.bak and delete any old *.log.bak with the
-#  same name:
-if os.path.isfile(LOG_FILE):
-    if os.path.isfile(LOG_FILE + ".bak"):
-        os.remove(LOG_FILE + ".bak")
 
+def checksigs():
+    # first, check to see if there is a log file with the same name as LOG_FILE,
+    #  if there is, rename it to *.log.bak and delete any old *.log.bak with the
+    #  same name:
     if os.path.isfile(LOG_FILE):
-        os.rename(LOG_FILE, LOG_FILE + ".bak")
-        #os.remove(LOG_FILE)
+        if os.path.isfile(LOG_FILE + ".bak"):
+            os.remove(LOG_FILE + ".bak")
+
+        if os.path.isfile(LOG_FILE):
+            os.rename(LOG_FILE, LOG_FILE + ".bak")
+            # os.remove(LOG_FILE)
 
     logger = open(LOG_FILE, 'w')
-    writeLog("Initialized the log file")
-else:
-    logger = open(LOG_FILE, 'w')
-    writeLog("Initialized the log file")
+    writelog("Initialized the log file", logger)
 
-writeLog("Running sigcheck on all certificate stores")
-process = Popen(['sigcheck.exe', '-t', '*', '-c'], stdout=PIPE)
+    writelog("Running sigcheck on all certificate stores", logger)
+    process = Popen(['sigcheck.exe', '-t', '*', '-c'], stdout=PIPE)
 
-(output, err) = process.communicate()
+    (output, err) = process.communicate()
 
-exit_code = process.wait()
+    exit_code = process.wait()
 
-if exit_code > 0:
-    # the return code was not normal, log the error code.
-    writeLog("The process did not exit with a known good code exited with: {}".format(exit_code))
-    exit()
+    if exit_code > 0:
+        # the return code was not normal, log the error code.
+        writelog("The process did not exit with a known good code exited with: {}".format(exit_code), logger)
+        exit()
+
+    # check for errors and an error code
+    if err:
+        writelog("An error has occurred \n", logger)
+        writelog(err)
+
+    writelog("Checking for previous instance of the results file", logger)
+
+    if os.path.isfile(RESULTS_FILE):
+        writelog("Deleting the old results file", logger)
+        os.remove(RESULTS_FILE)
+
+    writelog("Opening file to save current results to", logger)
+    f = open(RESULTS_FILE, 'w')
+
+    # first, split the output into an array of it's lines:
+    output = output.splitlines()
+
+    # now, write the contents to the last run file
+    for line in output:
+        f.write(line)
+        f.write('\n')
+
+    f.close()
+
+    f = open(RESULTS_FILE, 'r')
+
+    # now, I need to do the comparison...
+    # load up the reference file
+    # compare against what we just ran
+    # if there are issues, create a notification alert!
+
+    reffile = open(REF_FILE, 'r')
+
+    sha256ref = hashlib.sha256()
+    sha256gen = hashlib.sha256()
+
+    while True:
+        datagen = f.read(BUFF_SIZE)
+        if not datagen:
+            break
+        sha256gen.update(datagen)
+
+    while True:
+        dataref = reffile.read(BUFF_SIZE)
+        if not dataref:
+            break
+        sha256ref.update(dataref)
+
+    if sha256gen.hexdigest() == sha256ref.hexdigest():
+        writelog("The files have the same hash {0}".format(sha256gen.hexdigest()), logger)
+    else:
+        writelog("The files don't match, fire up the notification window", logger)
+        app = wx.App()
+        TaskBarIcon()
+        app.MainLoop()
+
+    writelog("Job completed, closing files...", logger)
+    # always remember to close the file!
+    f.close()
+    reffile.close()
+    logger.close()
 
 
-# check for errors and an error code
-if err:
-    writeLog("An error has occurred \n")
-    writeLog(err)
+def create_menu_item(menu, label, func):
+    item = wx.MenuItem(menu, -1, label)
+    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
+    menu.AppendItem(item)
+    return item
 
-writeLog("Checking for previous instance of the results file")
 
-if os.path.isfile(RESULTS_FILE):
-    writeLog("Deleting the old results file")
-    os.remove(RESULTS_FILE)
+class TaskBarIcon(wx.TaskBarIcon):
+    def __init__(self):
+        super(TaskBarIcon, self).__init__()
+        self.set_icon(TRAY_ICON)
+        self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
 
-writeLog("Opening file to save current results to")
-f = open(RESULTS_FILE, 'w')
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        create_menu_item(menu, 'Open Sigcheck Folder', self.on_open)
+        menu.AppendSeparator()
+        create_menu_item(menu, 'Exit', self.on_exit)
+        return menu
 
-# first, split the output into an array of it's lines:
-output = output.splitlines()
+    def set_icon(self, path):
+        icon = wx.IconFromBitmap(wx.Bitmap(path))
+        self.SetIcon(icon, TRAY_TOOLTIP)
 
-# now, write the contents to the last run file
-for line in output:
-    f.write(line)
-    f.write('\n')
+    @staticmethod
+    def on_left_down(event):
+        Popen('explorer ' + os.getcwd())
 
-# now, I need to do the comparison...
-# load up the reference file
-# compare against what we just ran
-# if there are issues, create a notification alert!
+    @staticmethod
+    def on_open(self, event):
+        Popen('explorer ' + os.getcwd())
 
-# I'm going to simply compare the two files, I have not had the certificates appear in a different order yet.
-#THIS DOESN'T WORK, I NEED TO DO SOMETHING ELSE HERE:
-if filecmp.cmp(REF_FILE, RESULTS_FILE):
-    writeLog("The certificate files are the same!  No changes in certificates from the reference file!")
-else:
-    writeLog("The files don't match!  Displaying an error message!")
-    # TODO: More to come here
+    def on_exit(self, event):
+        wx.CallAfter(self.Destroy)
 
-writeLog("Job completed, closing files...")
-# always remember to close the file!
-f.close()
-logger.close()
+
+def main():
+
+    checksigs()
+
+if __name__ == '__main__':
+    main()
